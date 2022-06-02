62e35d0baf4fd980bc3a6e843af24d992293e9b1
Gustavo Soares <gustavosoaresdev10@gmail.com>
2022-06-02 13:39:23 -0300

    messages e campo de pesquisa adicionados

--- contatos/views.py
+++ contatos/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from django.core.paginator import Paginator
 from .models import Contato
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
+from django.contrib import messages
 
 def index(request):
   contatos = Contato.objects.order_by('-id').filter(
@@ -22,3 +25,21 @@ def ver_contato(request, contato_id):
       'contato': contato
   })
   
+def busca(request):
+    termo = request.GET.get('termo')
+    if termo is None or not termo:
+      messages.add_message(request, messages.error, 'Campo termo não pode ficar vazio')
+      return redirect('index')
+      
+    campos = Concat('nome', Value(''), 'sobrenome')
+    contatos = Contato.objects.annotate(
+      nome_completo=campos
+    ).filter(
+      Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+    paginator = Paginator(contatos, 20)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html', {
+      'contatos': contatos
+    })
